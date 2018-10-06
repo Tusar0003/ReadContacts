c8d092c49bb87382e23856f214cdb94a174818e9
Tusar0003 <faisalahmedtusar@gmail.com>
2018-10-06 21:22:23 +0600

    Mod: New IP address added

--- app/src/main/java/com/example/no0ne/readcontacts/ApiClient.java
+++ app/src/main/java/com/example/no0ne/readcontacts/ApiClient.java
@@ -13,6 +13,7 @@ public class ApiClient {
 
     // 127.0.0.1
     // 10.0.2.2
+    // 192.168.58.1
     public static final String BASE_URL = "http://192.168.0.102:80/Android/";
     public static Retrofit retrofit = null;
 
